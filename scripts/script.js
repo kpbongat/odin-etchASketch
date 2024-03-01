3dde387a0496bebc54a6a6c8b2baeabf315f87a8
kpbongat <kpbongat.dev@gmail.com>
2024-03-01 19:41:14 +0800

    Add missing grid reference

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 const NUM_OF_SQUARES = 16;
+const grid = document.querySelector('.grid');
 
 for (let i = 0; i<NUM_OF_SQUARES; i++) {
-    const div = document.createElement('div')
-    grid.appendChild('div')
+    const div = document.createElement('div');
+    grid.appendChild('div';
 
 }
 
