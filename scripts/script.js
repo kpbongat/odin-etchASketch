f1c516302dea3e9318ed11f1296335cdb2436707
kpbongat <kpbongat.dev@gmail.com>
2024-03-02 17:06:26 +0800

    Seperate div creation for rows and columns of grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
-const NUM_OF_SQUARES = 16 * 16;
+const NUM_OF_ROWS = 16;
+const NUM_OF_COLUMNS = 16;
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i<NUM_OF_SQUARES; i++) {
-    const div = document.createElement('div');
-    grid.appendChild(div);
+for (let x = 0; x<NUM_OF_ROWS; x++) {
+    const rowDiv = document.createElement('div');
+    for (let y = 0; y<NUM_OF_COLUMNS; y++){
+        const columnDiv = document.createElement('div');
+        rowDiv.appendChild(columnDiv);
+    }
+    grid.appendChild(rowDiv);
 
 }
 
