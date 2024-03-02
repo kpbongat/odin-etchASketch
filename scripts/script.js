0387cf39efedc470cb86c46a7959530b5f5d41f3
kpbongat <kpbongat.dev@gmail.com>
2024-03-02 17:29:57 +0800

    Add hover event listeners for drawing effect

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,18 @@ for (let x = 0; x<NUM_OF_ROWS; x++) {
     for (let y = 0; y<NUM_OF_COLUMNS; y++){
         const columnDiv = document.createElement('div');
         columnDiv.classList.toggle('column');
+        columnDiv.addEventListener('mouseenter',(e)=>{
+            e.target.classList.toggle('hover')
+        });
+        columnDiv.addEventListener('mouseleave',(e)=>{
+            e.target.classList.toggle('hover')
+        });
+        rowDiv.addEventListener('mouseenter',(e)=>{
+            e.target.classList.toggle('hover')
+        });
+        rowDiv.addEventListener('mouseleave',(e)=>{
+            e.target.classList.toggle('hover')
+        });
         rowDiv.appendChild(columnDiv);
     }
     grid.appendChild(rowDiv);
